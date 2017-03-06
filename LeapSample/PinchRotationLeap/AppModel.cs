fb39022544a0065c6fe3d46d5bf0be3d3f25af9a
Keiho Sakapon <git@saka-pon.net>
2017-03-06 22:26:31 +0900

    Add properties for position

--- LeapSample/PinchRotationLeap/AppModel.cs
+++ LeapSample/PinchRotationLeap/AppModel.cs
@@ -13,11 +13,13 @@ namespace PinchRotationLeap
     {
         public Transform3D CubeTransform { get; }
         QuaternionRotation3D quaternionRotation = new QuaternionRotation3D();
+        TranslateTransform3D translateTransform = new TranslateTransform3D();
 
         public LeapManager LeapManager { get; } = new LeapManager();
 
         public ReadOnlyReactiveProperty<Leap.Hand> FrontHand { get; }
         public ReadOnlyReactiveProperty<Quaternion?> HandRotation { get; }
+        public ReadOnlyReactiveProperty<Vector3D?> HandPosition { get; }
         public ReadOnlyReactiveProperty<bool> IsPinched { get; }
 
         public IObservable<Quaternion> PinchRotateDelta { get; }
@@ -35,6 +37,9 @@ namespace PinchRotationLeap
                 .ToReadOnlyReactiveProperty();
             HandRotation = FrontHand
                 .Select(h => h?.GetEulerAngles().ToQuaternion())
+                .ToReadOnlyReactiveProperty();
+            HandPosition = FrontHand
+                .Select(h => h?.PalmPosition.ToVector3D())
                 .ToReadOnlyReactiveProperty();
             IsPinched = FrontHand
                 .Select(h => h?.PinchStrength == 1.0F)
@@ -66,6 +71,7 @@ namespace PinchRotationLeap
         {
             var transform = new Transform3DGroup();
             transform.Children.Add(new RotateTransform3D(quaternionRotation));
+            transform.Children.Add(translateTransform);
             return transform;
         }
     }
