5ec0a41b948afad2e470c2b021d635eb7aa09da6
Keiho Sakapon <git@saka-pon.net>
2017-05-25 15:57:46 +0900

    Add test

--- ProxySample/DynamicHttpConsole/Program.cs
+++ ProxySample/DynamicHttpConsole/Program.cs
@@ -7,31 +7,39 @@ namespace DynamicHttpConsole
     class Program
     {
         const string Uri_ZipCloud = "http://zipcloud.ibsnet.co.jp/api/search";
-        const string Uri_Cgis = "http://zip.cgis.biz/xml/zip.php";
+        const string Uri_Cgis_Xml = "http://zip.cgis.biz/xml/zip.php";
 
         static void Main(string[] args)
         {
+            HttpHelperTest0();
             HttpHelperTest();
             DynamicHttpProxyTest();
         }
 
-        static void HttpHelperTest()
+        static void HttpHelperTest0()
         {
             Console.WriteLine(HttpHelper.Get(Uri_ZipCloud));
-            Console.WriteLine(HttpHelper.Get(Uri_ZipCloud, new Dictionary<string, object> { { "zipcode", "6040973" } }));
-            Console.WriteLine(HttpHelper.Get(Uri_ZipCloud, new { zipcode = "6040973" }));
+            Console.WriteLine(HttpHelper.Get(Uri_ZipCloud, new { zipcode = "6020881" }));
+        }
 
-            Console.WriteLine(HttpHelper.Get(Uri_Cgis));
-            Console.WriteLine(HttpHelper.Get(Uri_Cgis, new { zn = "6020881" }));
+        static void HttpHelperTest()
+        {
+            Console.WriteLine(HttpHelper.Get(Uri_Cgis_Xml));
+            Console.WriteLine(HttpHelper.Get(Uri_Cgis_Xml, new Dictionary<string, object> { { "zn", "6048301" } }));
+            Console.WriteLine(HttpHelper.Get(Uri_Cgis_Xml, new Dictionary<string, object> { { "zn", "501" }, { "ver", 1 } }));
+            Console.WriteLine(HttpHelper.Get(Uri_Cgis_Xml, new { zn = "6050073" }));
+            Console.WriteLine(HttpHelper.Get(Uri_Cgis_Xml, new { zn = "502", ver = 1 }));
         }
 
         static void DynamicHttpProxyTest()
         {
             dynamic http = new DynamicHttpProxy();
 
-            Console.WriteLine(http.Get(Uri_Cgis));
-            Console.WriteLine(http.Get(Uri_Cgis, new { zn = "6020881" }));
-            Console.WriteLine(http.Get(Uri_Cgis, zn: "6020881"));
+            Console.WriteLine(http.Get(Uri_Cgis_Xml));
+            Console.WriteLine(http.Get(Uri_Cgis_Xml, new { zn = "1000001" }));
+            Console.WriteLine(http.Get(Uri_Cgis_Xml, new { zn = "401", ver = 1 }));
+            Console.WriteLine(http.Get(Uri_Cgis_Xml, zn: "1510052"));
+            Console.WriteLine(http.Get(Uri_Cgis_Xml, zn: "402", ver: 1));
         }
     }
 }
