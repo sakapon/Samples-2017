e23cbdb5377a16b6b33e8977c8b9b2adc1ee67c5
Keiho Sakapon <git@saka-pon.net>
2017-06-19 22:23:57 +0900

    Add test

--- ProxySample/TransparentHttpConsole/Program.cs
+++ ProxySample/TransparentHttpConsole/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TransparentHttpConsole
 {
@@ -10,6 +8,25 @@ namespace TransparentHttpConsole
     {
         static void Main(string[] args)
         {
+            var zipCloud = HttpProxy.CreateProxy<IZipCloudService>();
+            Console.WriteLine(zipCloud.Get("6020881"));
+
+            var cgis = HttpProxy.CreateProxy<ICgisService>();
+            Console.WriteLine(cgis.Get("6048301"));
+            Console.WriteLine(cgis.Get("501", 1));
         }
+    }
+
+    [BaseUri("http://zipcloud.ibsnet.co.jp/api/search")]
+    public interface IZipCloudService
+    {
+        string Get(string zipcode);
+    }
+
+    [BaseUri("http://zip.cgis.biz/xml/zip.php")]
+    public interface ICgisService
+    {
+        string Get(string zn);
+        string Get(string zn, int ver);
     }
 }
