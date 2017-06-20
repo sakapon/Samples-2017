6c85a461512ec8b7f2fd69542e7a59dc8782efcd
Keiho Sakapon <git@saka-pon.net>
2017-06-20 19:33:09 +0900

    Add test

--- ProxySample/TransparentHttpConsole/Program.cs
+++ ProxySample/TransparentHttpConsole/Program.cs
@@ -8,12 +8,28 @@ namespace TransparentHttpConsole
     {
         static void Main(string[] args)
         {
+            HttpProxyTest();
+            ObjectMethodsTest();
+        }
+
+        static void HttpProxyTest()
+        {
             var zipCloud = HttpProxy.CreateProxy<IZipCloudService>();
             Console.WriteLine(zipCloud.Get("6020881"));
 
             var cgis = HttpProxy.CreateProxy<ICgisService>();
             Console.WriteLine(cgis.Get("6048301"));
             Console.WriteLine(cgis.Get("501", 1));
+        }
+
+        static void ObjectMethodsTest()
+        {
+            var zipCloud = HttpProxy.CreateProxy<IZipCloudService>();
+
+            var h = zipCloud.GetHashCode();
+            var b = zipCloud.Equals(zipCloud);
+            var t = zipCloud.GetType();
+            var s = zipCloud.ToString();
         }
     }
 
