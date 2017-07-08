37f52e5ea6d1aa1571edae3dfb2f9b1afa44b031
Keiho Sakapon <git@saka-pon.net>
2017-07-08 19:03:49 +0900

    Implement receiver

--- Network/UdpSample/UdpReceiver/Program.cs
+++ Network/UdpSample/UdpReceiver/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,41 @@ namespace UdpReceiver
 {
     class Program
     {
+        const int ReceiverPort = 8100;
+
         static void Main(string[] args)
         {
+            Task.Run(() =>
+            {
+                var client = new UdpClient(ReceiverPort);
+                var senderEP = default(IPEndPoint);
+
+                while (true)
+                {
+                    try
+                    {
+                        var data = client.Receive(ref senderEP);
+                        var text = Encoding.UTF8.GetString(data);
+
+                        Console.WriteLine($"{senderEP} {DateTime.Now:HH:mm:ss.fff}: {text}");
+                    }
+                    catch (SocketException ex)
+                    {
+                        switch (ex.SocketErrorCode)
+                        {
+                            case SocketError.ConnectionReset:
+                                continue;
+                            case SocketError.Interrupted:
+                                return;
+                            default:
+                                throw;
+                        }
+                    }
+                }
+            });
+
+            Console.WriteLine("Press [Enter] key to exit.");
+            Console.ReadLine();
         }
     }
 }
