0c0be216c83f2cc6833d27a8b4ce15b534c74ecc
Keiho Sakapon <git@saka-pon.net>
2017-07-10 11:52:40 +0900

    Show local IP addresses

--- Network/UdpSample/UdpMessenger/Program.cs
+++ Network/UdpSample/UdpMessenger/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 
 namespace UdpMessenger
 {
@@ -8,12 +10,16 @@ namespace UdpMessenger
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Local Addresses:");
+            foreach (var address in GetHostAddresses())
+                Console.WriteLine(address);
+
+            var localPort = int.Parse(GetInput("Local Port: ", "8100"));
+            var remoteHost = GetInput("Remote Address: ", "localhost");
+            var remotePort = int.Parse(GetInput("Remote Port: ", "8101"));
+
             try
             {
-                var localPort = int.Parse(GetInput("Local Port: ", "8100"));
-                var remoteHost = GetInput("Remote Host: ", "localhost");
-                var remotePort = int.Parse(GetInput("Remote Port: ", "8101"));
-
                 using (var client = new TextUdpClient(localPort, remoteHost, remotePort))
                 {
                     client.TextReceived += s => Console.WriteLine($"Received: {s}");
@@ -33,6 +39,10 @@ namespace UdpMessenger
                 Console.WriteLine(ex.Message);
             }
         }
+
+        static IEnumerable<IPAddress> GetHostAddresses() =>
+            Dns.GetHostAddresses(Dns.GetHostName())
+                .Where(a => a.AddressFamily == AddressFamily.InterNetwork);
 
         static string GetInput(string message = "", string defaultValue = "")
         {
