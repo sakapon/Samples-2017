409dc425c038b992bd1d439bdacb08369a6a7fba
Keiho Sakapon <git@saka-pon.net>
2017-07-11 17:54:40 +0900

    Scale bitmap from screen

--- DrawingSample/BitmapScaleConsole/Program.cs
+++ DrawingSample/BitmapScaleConsole/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BitmapScaleConsole
 {
@@ -10,6 +9,19 @@ namespace BitmapScaleConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press [Enter] key to start.");
+            Console.ReadLine();
+
+            var now = $"{DateTime.Now:yyyyMMdd-HHmmss}";
+            var fileName = $"{now}.png";
+            var fileName_scaled = $"{now}-Scaled.jpg";
+
+            using (var bitmap = BitmapHelper.GetScreenBitmap(200, 100, 1200, 800))
+            using (var scaled = BitmapHelper.ScaleImage(bitmap, 600, 400))
+            {
+                bitmap.Save(fileName, ImageFormat.Png);
+                scaled.Save(fileName_scaled, ImageFormat.Jpeg);
+            }
         }
     }
 }
