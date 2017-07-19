500a86a65d96c025234f237272aadc147393babf
Keiho Sakapon <git@saka-pon.net>
2017-07-19 18:48:44 +0900

    Add classes

---
+++ ProxySample/CrossCuttingConsole/CrossCuttingProxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+
+namespace CrossCuttingConsole
+{
+    public class CrossCuttingProxy : RealProxy
+    {
+        public override IMessage Invoke(IMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public abstract class AspectAttribute : Attribute
+    {
+        public abstract IMethodReturnMessage Invoke(Func<IMethodReturnMessage> baseInvoke, MarshalByRefObject target, IMethodCallMessage methodCall);
+    }
+}
