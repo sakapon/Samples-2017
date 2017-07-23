b4d1cf95638932a0d0671d62c0d1bd5d5b6d85c0
Keiho Sakapon <git@saka-pon.net>
2017-07-24 01:07:48 +0900

    Add test

--- ProxySample/ProxyableConsole/Program.cs
+++ ProxySample/ProxyableConsole/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Transactions;
 
 namespace ProxyableConsole
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            AspectTest();
+            NorthwindBusinessTest();
+        }
+
+        static void AspectTest()
         {
             var result = Proxyable.Body(() =>
                 {
@@ -42,5 +49,44 @@ namespace ProxyableConsole
                 })
                 .Execute();
         }
+
+        static void NorthwindBusinessTest()
+        {
+            var units = NorthwindBusiness.SelectUnitsInStock();
+            NorthwindBusiness.InsertCategory("Books");
+            try
+            {
+                NorthwindBusiness.ErrorTest();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+
+    public static class NorthwindBusiness
+    {
+        // cf. https://sakapon.wordpress.com/2011/10/02/dirtyread2/
+        public static int SelectUnitsInStock() =>
+            Proxyable.Body(() =>
+                {
+                    Console.WriteLine("SelectUnitsInStock");
+                    return 123;
+                })
+                .TransactionScope()
+                .TraceLog()
+                .Execute();
+
+        // cf. https://sakapon.wordpress.com/2011/12/14/phantomread2/
+        public static void InsertCategory(string name) =>
+            Proxyable.Body(() => Console.WriteLine("InsertCategory"))
+                .TransactionScope(IsolationLevel.Serializable)
+                .TraceLog()
+                .Execute();
+
+        public static void ErrorTest() =>
+            Proxyable.Body(() => throw new InvalidOperationException("This is an error test."))
+                .TraceLog()
+                .Execute();
     }
 }
