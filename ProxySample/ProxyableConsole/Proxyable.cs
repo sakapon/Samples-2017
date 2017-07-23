76163a882d51fd0de38039e85ccfa371b1bed7d7
Keiho Sakapon <git@saka-pon.net>
2017-07-23 20:45:44 +0900

    Add classes

--- ProxySample/ProxyableConsole/Proxyable.cs
+++ ProxySample/ProxyableConsole/Proxyable.cs
@@ -5,7 +5,9 @@ namespace ProxyableConsole
     public static class Proxyable
     {
         public static IProxyable<TResult> Body<TResult>(Func<TResult> execute) => new BodyProxyable<TResult>(execute);
+        public static IProxyable Body(Action execute) => new BodyProxyable(execute);
 
         public static IProxyable<TResult> Aspect<TResult>(this IProxyable<TResult> source, Func<Func<TResult>, TResult> execute) => new AspectProxyable<TResult>(source, execute);
+        public static IProxyable Aspect(this IProxyable source, Action<Action> execute) => new AspectProxyable(source, execute);
     }
 }
