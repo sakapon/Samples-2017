8b9b9e3150a15c036fd94a54ba87ab74f5539281
Keiho Sakapon <git@saka-pon.net>
2017-07-23 20:31:36 +0900

    Add test

--- ProxySample/ProxyableConsole/Program.cs
+++ ProxySample/ProxyableConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProxyableConsole
 {
@@ -10,6 +6,26 @@ namespace ProxyableConsole
     {
         static void Main(string[] args)
         {
+            var result = Proxyable.Body(() =>
+                {
+                    Console.WriteLine("Body");
+                    return 123;
+                })
+                .Aspect(f =>
+                {
+                    Console.WriteLine("Before");
+                    var r = f();
+                    Console.WriteLine("After");
+                    return r;
+                })
+                .Aspect(f =>
+                {
+                    Console.WriteLine("Begin");
+                    var r = f();
+                    Console.WriteLine("End");
+                    return r;
+                })
+                .Execute();
         }
     }
 }
