1fdc578516687a8b0ec8559a0e10bd8ef9461650
Keiho Sakapon <git@saka-pon.net>
2017-07-23 20:13:52 +0900

    Add classes

---
+++ ProxySample/ProxyableConsole/IProxyable_TResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProxyableConsole
+{
+    public interface IProxyable<TResult>
+    {
+        TResult Execute();
+    }
+
+    class BodyProxyable<TResult> : IProxyable<TResult>
+    {
+        Func<TResult> execute;
+
+        public BodyProxyable(Func<TResult> execute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public TResult Execute() => execute();
+    }
+
+    class AspectProxyable<TResult> : IProxyable<TResult>
+    {
+        IProxyable<TResult> source;
+        Func<Func<TResult>, TResult> execute;
+
+        public AspectProxyable(IProxyable<TResult> source, Func<Func<TResult>, TResult> execute)
+        {
+            this.source = source ?? throw new ArgumentNullException(nameof(source));
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public TResult Execute() => execute(source.Execute);
+    }
+}
---
+++ ProxySample/ProxyableConsole/Proxyable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProxyableConsole
+{
+    public static class Proxyable
+    {
+        public static IProxyable<TResult> Body<TResult>(Func<TResult> execute) => new BodyProxyable<TResult>(execute);
+
+        public static IProxyable<TResult> Aspect<TResult>(this IProxyable<TResult> source, Func<Func<TResult>, TResult> execute) => new AspectProxyable<TResult>(source, execute);
+    }
+}
