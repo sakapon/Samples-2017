99922620236787d2e12657f13083c3aae36efc58
Keiho Sakapon <git@saka-pon.net>
2017-07-26 18:27:10 +0900

    Add methods

--- DrawingSample/BitmapScaleConsole/BitmapHelper.cs
+++ DrawingSample/BitmapScaleConsole/BitmapHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace BitmapScaleConsole
 {
@@ -17,6 +19,9 @@ namespace BitmapScaleConsole
             return bitmap;
         }
 
+        public static Bitmap ResizeImage(Image source, Size size, InterpolationMode interpolationMode = InterpolationMode.Bilinear) =>
+            ResizeImage(source, size.Width, size.Height, interpolationMode);
+
         // Default = Low = Bilinear
         // High = HighQualityBicubic
         public static Bitmap ResizeImage(Image source, int width, int height, InterpolationMode interpolationMode = InterpolationMode.Bilinear)
@@ -29,6 +34,16 @@ namespace BitmapScaleConsole
                 graphics.DrawImage(source, 0, 0, width, height);
             }
             return bitmap;
+        }
+
+        public static Stream ToStream(Bitmap bitmap)
+        {
+            var memory = new MemoryStream();
+
+            bitmap.Save(memory, ImageFormat.Bmp);
+
+            memory.Position = 0;
+            return memory;
         }
     }
 }
