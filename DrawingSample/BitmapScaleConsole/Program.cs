23f5e042836287501ae8752b0217119f95f0a369
Keiho Sakapon <git@saka-pon.net>
2017-07-26 18:29:10 +0900

    Add test

--- DrawingSample/BitmapScaleConsole/Program.cs
+++ DrawingSample/BitmapScaleConsole/Program.cs
@@ -14,6 +14,12 @@ namespace BitmapScaleConsole
             Console.WriteLine("Press [Enter] key to start.");
             Console.ReadLine();
 
+            ResizeImageTest_Drawing();
+            ResizeImageTest_Windows();
+        }
+
+        static void ResizeImageTest_Drawing()
+        {
             var now = $"{DateTime.Now:yyyyMMdd-HHmmss}";
             Directory.CreateDirectory(now);
 
@@ -32,6 +38,25 @@ namespace BitmapScaleConsole
                         resized.Save($@"{now}\{mode}.jpg", ImageFormat.Jpeg);
                     }
                 }
+            }
+        }
+
+        static void ResizeImageTest_Windows()
+        {
+            var now = $"{DateTime.Now:yyyyMMdd-HHmmss}";
+            Directory.CreateDirectory(now);
+
+            using (var bitmap = BitmapHelper.GetScreenBitmap(200, 100, 1080, 720))
+            using (var memory = BitmapHelper.ToStream(bitmap))
+            {
+                bitmap.Save($@"{now}\Original.png", ImageFormat.Png);
+                bitmap.Save($@"{now}\Original.jpg", ImageFormat.Jpeg);
+
+                var source = System.Windows.Media.Imaging.BitmapFrame.Create(memory);
+                var resized = BitmapHelper2.ResizeImage(source, bitmap.Width / 2, bitmap.Height / 2);
+
+                BitmapHelper2.SaveImage($@"{now}\Resized.png", resized);
+                BitmapHelper2.SaveImage($@"{now}\Resized.jpg", resized);
             }
         }
     }
