8aa7828c9f093730d6110594852345d85a54a960
Keiho Sakapon <git@saka-pon.net>
2017-09-18 23:17:54 +0900

    Add classes

---
+++ MathSample/DecimalConsole/EnumerableHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DecimalConsole
+{
+    public static class EnumerableHelper
+    {
+        public static string ToSimpleString<TSource>(this IEnumerable<TSource> source) => string.Join("", source);
+    }
+}
---
+++ MathSample/DecimalConsole/URealDecimal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DecimalConsole
+{
+    struct URealDecimal
+    {
+        static readonly IDictionary<char, byte> DigitsMap = Enumerable.Range(0, 10).ToDictionary(i => i.ToString()[0], i => (byte)i);
+        static readonly byte[] _digits_empty = new byte[0];
+
+        byte[] _digits;
+        byte[] Digits => _digits ?? _digits_empty;
+
+        public int? Degree { get; }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (!Degree.HasValue) return 0;
+                var i = Degree.Value - index;
+                return (0 <= i || i < Digits.Length) ? Digits[i] : 0;
+            }
+        }
+
+        URealDecimal(byte[] digits, int? degree = null)
+        {
+            if (digits == null || digits.Length == 0)
+            {
+                if (degree.HasValue) throw new ArgumentException("");
+            }
+            else
+            {
+                if (digits[0] == 0) throw new ArgumentException("");
+                if (digits[digits.Length - 1] == 0) throw new ArgumentException("");
+                if (!degree.HasValue) throw new ArgumentException("");
+            }
+
+            _digits = digits;
+            Degree = degree;
+        }
+    }
+}
