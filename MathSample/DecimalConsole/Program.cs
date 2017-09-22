0f05475fc2c4f7aadee4ccc2305aaa18b30bc36b
Keiho Sakapon <git@saka-pon.net>
2017-09-22 15:22:28 +0900

    Add test

--- MathSample/DecimalConsole/Program.cs
+++ MathSample/DecimalConsole/Program.cs
@@ -9,6 +9,7 @@ namespace DecimalConsole
         static void Main(string[] args)
         {
             URealDecimal_ToString();
+            URealDecimal_Add();
 
             EnumerableHelperTest();
         }
@@ -25,6 +26,24 @@ namespace DecimalConsole
             Test(new byte[] { 1, 2, 3 }, -1);
             Test(new byte[] { 1, 2, 3 }, -3);
             Test(new byte[] { 1, 2, 3, 4 }, 1);
+        }
+
+        static void URealDecimal_Add()
+        {
+            URealDecimal_Add("0", "0", "0");
+            URealDecimal_Add("1", "0", "1");
+            URealDecimal_Add("3", "1", "2");
+            URealDecimal_Add("1.2", "1", "0.2");
+            URealDecimal_Add("10", "1", "9");
+            URealDecimal_Add("20", "11", "9");
+            URealDecimal_Add("1", "0.8", "0.2");
+        }
+
+        static void URealDecimal_Add(URealDecimal expected, URealDecimal d1, URealDecimal d2)
+        {
+            var actual = d1 + d2;
+            if (expected != actual)
+                Console.WriteLine($"{expected} != {d1} + {d2}");
         }
 
         static void EnumerableHelperTest()
