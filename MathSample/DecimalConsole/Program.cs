0231364810056383548ad9bb3d6c7d68d4ee56de
Keiho Sakapon <git@saka-pon.net>
2017-09-23 17:54:04 +0900

    Refactor

--- MathSample/DecimalConsole/Program.cs
+++ MathSample/DecimalConsole/Program.cs
@@ -34,64 +34,66 @@ namespace DecimalConsole
 
         static void URealDecimal_Add()
         {
-            URealDecimal_Add("0", "0", "0");
-            URealDecimal_Add("1", "0", "1");
-            URealDecimal_Add("3", "1", "2");
-            URealDecimal_Add("1.2", "1", "0.2");
-            URealDecimal_Add("10", "1", "9");
-            URealDecimal_Add("20", "11", "9");
-            URealDecimal_Add("1", "0.8", "0.2");
+            void Test(URealDecimal expected, URealDecimal d1, URealDecimal d2)
+            {
+                if (expected != d1 + d2) WriteLine($"{expected} != {d1} + {d2}");
+            }
+
+            Test("0", "0", "0");
+            Test("1", "0", "1");
+            Test("3", "1", "2");
+            Test("1.2", "1", "0.2");
+            Test("130.42", "100.02", "30.4");
+            Test("10", "1", "9");
+            Test("20", "11", "9");
+            Test("1", "0.8", "0.2");
         }
 
         static void URealDecimal_Subtract()
         {
-            URealDecimal_Subtract("0", "0", "0");
-            URealDecimal_Subtract("1", "1", "0");
-            URealDecimal_Subtract("2", "3", "1");
-            URealDecimal_Subtract("1", "1.2", "0.2");
-            URealDecimal_Subtract("9", "10", "1");
-            URealDecimal_Subtract("11", "20", "9");
-            URealDecimal_Subtract("0.8", "1", "0.2");
+            void Test(URealDecimal expected, URealDecimal d1, URealDecimal d2)
+            {
+                if (expected != d1 - d2) WriteLine($"{expected} != {d1} - {d2}");
+            }
+
+            Test("0", "0", "0");
+            Test("1", "1", "0");
+            Test("2", "3", "1");
+            Test("1", "1.2", "0.2");
+            Test("9", "10", "1");
+            Test("11", "20", "9");
+            Test("0.8", "1", "0.2");
         }
 
         static void URealDecimal_Multiply()
         {
-            URealDecimal_Multiply("0", "0", "0");
-            URealDecimal_Multiply("0", "1", "0");
-            URealDecimal_Multiply("3", "3", "1");
-            URealDecimal_Multiply("0.6", "2", "0.3");
-            URealDecimal_Multiply("323", "17", "19");
-            URealDecimal_Multiply("0.00018", "0.02", "0.009");
-            URealDecimal_Multiply("1", "32", "0.03125");
+            void Test(URealDecimal expected, URealDecimal d1, URealDecimal d2)
+            {
+                if (expected != d1 * d2) WriteLine($"{expected} != {d1} * {d2}");
+            }
+
+            Test("0", "0", "0");
+            Test("0", "1", "0");
+            Test("3", "3", "1");
+            Test("0.6", "2", "0.3");
+            Test("323", "17", "19");
+            Test("0.00018", "0.02", "0.009");
+            Test("485.45663", "60.007", "8.09");
+            Test("1", "32", "0.03125");
         }
 
         static void URealDecimal_Power()
         {
-            URealDecimal_Power("0", "0", 1);
-            URealDecimal_Power("1", "2", 0);
-            URealDecimal_Power("81", "3", 4);
-            URealDecimal_Power("0.001", "0.1", 3);
-            URealDecimal_Power("65536", "2", 16);
-        }
+            void Test(URealDecimal expected, URealDecimal d, int power)
+            {
+                if (expected != (d ^ power)) WriteLine($"{expected} != {d} ^ {power}");
+            }
 
-        static void URealDecimal_Add(URealDecimal expected, URealDecimal d1, URealDecimal d2)
-        {
-            if (expected != d1 + d2) WriteLine($"{expected} != {d1} + {d2}");
-        }
-
-        static void URealDecimal_Subtract(URealDecimal expected, URealDecimal d1, URealDecimal d2)
-        {
-            if (expected != d1 - d2) WriteLine($"{expected} != {d1} - {d2}");
-        }
-
-        static void URealDecimal_Multiply(URealDecimal expected, URealDecimal d1, URealDecimal d2)
-        {
-            if (expected != d1 * d2) WriteLine($"{expected} != {d1} * {d2}");
-        }
-
-        static void URealDecimal_Power(URealDecimal expected, URealDecimal d, int power)
-        {
-            if (expected != (d ^ power)) WriteLine($"{expected} != {d} ^ {power}");
+            Test("0", "0", 1);
+            Test("1", "2", 0);
+            Test("81", "3", 4);
+            Test("0.001", "0.1", 3);
+            Test("65536", "2", 16);
         }
 
         static void EnumerableHelperTest()
